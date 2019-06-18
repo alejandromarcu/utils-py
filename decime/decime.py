2dc636f17727589827d47343da1a4e585a9c12bd
Alejandro Marcu <amarcu@gmail.com>
2019-06-18 11:20:59 -0700

    Added decime

---
+++ decime/decime.py
@@ -0,0 +1,54 @@
+import sys
+from lxml import html
+import requests
+import os
+from playsound import playsound
+from shutil import copyfile
+from tempfile import gettempdir
+import time
+import re
+
+save_path = "C:\\Cucu\\tmp\\wiktionary"
+is_az = re.compile("[a-zA-Z]*")
+lang = "Ru"
+
+while True:
+    word = input("Word: ")
+
+    if word == "x":
+        break
+
+    if is_az.match(word).span() == (0, 0):
+        lang = "Ru"
+        no_accents = word.encode().replace(b'\xcc\x81',b'').decode()
+        if word != no_accents:
+            word = no_accents
+            print("Accents removed: " + word)
+    else:
+        lang = "En-us"
+        word = word.lower()
+
+    url = "https://commons.wikimedia.org/w/index.php?title=File%3A{}-{}.ogg".format(lang, word)
+
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+
+    download = tree.xpath('//a[@title="Download file"]')
+    if not download:
+        print("Can't find the entry for {}".format(word))
+        continue
+
+    download_link = download[0].get("href")
+
+    r = requests.get(download_link, allow_redirects=True)
+    filename = "{}\\{}.mp3".format(save_path, word)
+    with open(filename, 'wb') as file:
+        file.write(r.content)
+
+    tmp_file = os.path.join(gettempdir(), "wiktionary_{}_tmp.mp3".format(int(time.time())))
+    copyfile(filename, tmp_file)
+    playsound(tmp_file, True)
+    os.unlink(tmp_file)
+    
+    print("Saved {}".format(filename))
+    
